b29a6d618b93f5a59d808413d471cd38f171d21d
Jazmin Jahaziel <jazmin.jahazieltru@gmail.com>
2021-04-03 22:08:52 -0300

    ajuste para peployment6

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
+
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-
-// crear servidor
+// crear el servidor
 const app = express();
 
-// Conectar a la DB
+// Conectar a la base de datos
 conectarDB();
 
 // habilitar cors
 app.use(cors());
 
 // Habilitar express.json
-app.use( express.json({ exdended: true }));
+app.use( express.json({ extended: true }));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
-
 
 // arrancar la app
 app.listen(port, '0.0.0.0', () => {
